21f74f0ca7e33de5cba614a7077485e8333f91f6
Anzhumani <psibia.game@gmail.com>
2022-10-28 11:43:25 +0700

    Add tvisual component and change backand methods

--- TextInfo/MainWindow.xaml.cs
+++ TextInfo/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TextInfo
 {
@@ -34,6 +35,7 @@ namespace TextInfo
             if (result == true)
             {
                 textInFile = File.ReadAllText(dialog.FileName);
+                TextInput.Text = textInFile;
             }
         }
         static string SortString(string input, bool reverse) // Этот метод выполняет сортировку строки. Если true - по убыванию, иначе - по возрастанию
@@ -49,24 +51,52 @@ namespace TextInfo
         {
             if(textInFile is not null)
                 textInFile = SortString(textInFile, false);
-            MessageBox.Show(textInFile);
+            TextInput.Text = textInFile;
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e) // Сортировка по убыванию
         {
             if (textInFile is not null)
                 textInFile = SortString(textInFile, true);
-            MessageBox.Show(textInFile);
+            TextInput.Text = textInFile;
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e) 
         {
-
+            if (textInFile is not null)
+            {
+                string[] textArray = textInFile.Split(new char[] { ' ' }); //разбиваем текст на слова (в массив строк)
+                MessageBox.Show($"Количество символов в тексте: {textInFile.Length}" +
+                    $"\nКоличество слов в тексте: {textArray.Length+1}");
+            } else {MessageBox.Show("Необходимо выбрать файл, чтобы получить информацию"); }
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e) // Найти элемент 
         {
+            if (TextPole.Text is not null && textInFile.Contains(TextPole.Text))
+                MessageBox.Show($"В тексте есть строка {TextPole.Text}");
+            else MessageBox.Show($"В тексте НЕТ строки {TextPole.Text}");
+        }
 
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            if (textInFile is not null)
+            {
+                Microsoft.Win32.SaveFileDialog save = new Microsoft.Win32.SaveFileDialog
+                {
+                    Filter = "TXT Files (*.png)|*.txt"
+                };
+                if (save.ShowDialog() == true)
+                {
+                    File.WriteAllText(save.FileName, textInFile, Encoding.Default);
+                    MessageBox.Show("Файл успешно сохранен!");
+                }
+            }
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            textInFile = TextInput.Text;
         }
     }
 }
